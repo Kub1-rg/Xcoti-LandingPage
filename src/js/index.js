44ba1e2af54f850b5d4e374aac99b5a817af89a4
Ericksena12 <85752520+Kub1-rg@users.noreply.github.com>
2024-08-09 22:47:41 -0500

    Ajustes Gifs

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,30 @@
-$(document).ready(function(){
-  
+$(document).ready(function() {
+  $('#españolBandera').on('click', function(e) {
+      e.preventDefault();
+      console.log('Hola');
 
+      // Realizar una solicitud AJAX para cargar el archivo JSON
+      $.getJSON('./src/json/es.json', function(data) {
+        console.log(data);
+        
+
+          // Actualizar el contenido de la página con las traducciones
+          $('#primerTitulo').text(data.primerTitulo);
+          $('#segundoTitulo').text(data.segundoTitulo);
+          $('#description').text(data.description);
+          // Actualiza otros elementos según sea necesario
+      }).fail(function() {
+          console.error('No se pudo cargar el archivo JSON.');
+      });
   });
+
+  $('#inglesBandera').on('click', function(e){
+    $.getJSON('./src/json/en.json', function (data) {
+      $('#primerTitulo').text(data.primerTitulo);
+      $('#segundoTitulo').text(data.segundoTitulo)
+    }).fail(function(){
+      console.error('No se pudo cargar el archivo JSON.');
+
+    })
+  });
+});
